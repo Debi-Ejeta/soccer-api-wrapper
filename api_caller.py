72ec443faea38b1611678de10bace0b2a9029922
Debi Ejeta <debimulugeta1999@gmail.com>
2023-03-04 17:53:00 -0500

    resolved api_caller

--- api_caller.py
+++ api_caller.py
@@ -10,35 +10,56 @@ Third iteration: having a script running every minute with api call and updating
 
 """
 
-api_token = '2860fa567cd942c3a7222e0b94a741b1'
+api_token = 'XXXXXXXXXXXXXXXXXXX'
+team_id_mapping = {
+    57: 'Arsenal',
+    65: 'Manchester City',
+    66: 'Manchester United',
+    73: 'Tottenham Hotspur',
+    67: 'Newcastle United',
+    63: 'Fulham', 
+    64: 'Liverpool',
+    397: 'Brighton & Hove Albion',
+    402: 'Brentford',
+    61: 'Chelsea',
+    58: 'Aston Villa',
+    354: 'Crystal Palace',
+    351: 'Nottingham Forest',
+    338: 'Leicester City',
+    76: 'Wolverhampton Wanderers',
+    563: 'West Ham United',
+    341: 'Leeds United',
+    62: 'Everton',
+    1044: 'Bournemouth',
+    340: 'Southampton'
+}
 
-def get_matches():
+def get_recent_matches():
     uri = 'https://api.football-data.org/v4/matches'
     headers = { 'X-Auth-Token': api_token }
     response = requests.get(uri, headers=headers)
     return response.json()
 
-def get_team_matches(team):
-    team_mapping = {86: "Real Madrid"}
-    id = get_key(team, team_mapping) # Just for real madrid now. Doesn't map team to ID
+def get_epl_team_matches(team):
+    id = get_key(team, team_id_mapping) 
     uri = f'https://api.football-data.org/v4/teams/{id}/matches?status=SCHEDULED'
     headers = { 'X-Auth-Token': api_token }
     response = requests.get(uri, headers=headers)
     return response.json()
 
-def get_team_standings():
+def get_epl_team_standings():
     uri = 'https://api.football-data.org/v4/competitions/PL/standings'
     headers = { 'X-Auth-Token': api_token }
     response = requests.get(uri, headers=headers)
-    return response.json()
+    return response.json()["standings"][0]["table"]
 
-def get_top_scorers():
+def get_epl_top_scorers():
     uri = 'https://api.football-data.org/v4/competitions/PL/scorers'
     headers = { 'X-Auth-Token': api_token }
     response = requests.get(uri, headers=headers)
-    return response.json()
+    return response.json()["scorers"]
 
-def get_matchday(matchday):
+def get_epl_matchday(matchday):
     uri = f'https://api.football-data.org/v4/competitions/PL/matches?matchday={matchday}'
     headers = { 'X-Auth-Token': api_token }
     response = requests.get(uri, headers=headers)
@@ -48,6 +69,3 @@ def get_key(val, mapping):
     for key, value in mapping.items():
         if val == value:
             return key
-
-if __name__ == "__main__":
-    get_team_matches("Real Madrid")
