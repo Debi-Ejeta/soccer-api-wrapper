93e91549bd2769b3b1d76e60975b09685133aca7
Debi Ejeta <debimulugeta1999@gmail.com>
2023-03-03 23:08:58 -0500

    added unit tests
    added integration test

---
+++ tests/api_caller_test.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest.mock import Mock, patch
+import sys
+import json
+sys.path.insert(1, '../soccer-api-wrapper')
+import api_caller
+
+class Tests(unittest.TestCase):
+    def test_get_recent_matches(self):
+        with patch('api_caller.requests.get') as mock_get:
+            api_caller.get_recent_matches()
+            self.assertEqual(mock_get.called, True)
+    
+    def test_get_epl_team_matches(self):
+        with patch('api_caller.requests.get') as mock_get:
+            api_caller.get_epl_team_matches("test")
+            self.assertEqual(mock_get.called, True)
+    
+    def test_get_epl_team_matches_for_a_team(self):
+        with patch('api_caller.requests.get') as mock_get:
+            mock_get.return_value.json().text = {
+                "id": "57",
+                "team": "Arsenal",
+                "matchday": "14",
+                "opposition": "Manchester City"
+            }
+            response = api_caller.get_epl_team_matches("Arsenal")
+            self.assertEqual(response.text["id"], "57")
+            self.assertEqual(response.text["team"], "Arsenal")
+            self.assertEqual(response.text["matchday"], "14")
+            self.assertEqual(response.text["opposition"], "Manchester City")
+
+    def test_get_epl_team_standings(self):
+        with patch('api_caller.requests.get') as mock_get:
+            api_caller.get_epl_team_standings()
+            self.assertEqual(mock_get.called, True)
+
+    def test_get_epl_top_scorers(self):
+        with patch('api_caller.requests.get') as mock_get:
+            api_caller.get_epl_top_scorers()
+            self.assertEqual(mock_get.called, True)
+    
+    def test_get_epl_matchday(self):
+        with patch('api_caller.requests.get') as mock_get:
+            api_caller.get_epl_matchday(23)
+            self.assertEqual(mock_get.called, True)
