7dec2ae42df27fdca9531ca46624b6561d0268fc
Debi Ejeta <debimulugeta1999@gmail.com>
2023-03-13 02:30:46 -0400

    resolved more errors

--- soccer_api_wrapper/tests/api_caller_test.py
+++ soccer_api_wrapper/tests/api_caller_test.py
@@ -1,10 +1,4 @@
-from soccer_api_wrapper import (
-    get_recent_matches,
-    get_epl_team_matches,
-    get_epl_team_standings,
-    get_epl_top_scorers,
-    get_epl_matchday,
-)
+from soccer_api_wrapper import api_caller
 import unittest
 from unittest.mock import patch
 import sys
@@ -15,12 +9,12 @@ sys.path.insert(1, '../soccer-api-wrapper')
 class Tests(unittest.TestCase):
     def test_get_recent_matches(self):
         with patch('api_caller.requests.get') as mock_get:
-            get_recent_matches()
+            api_caller.get_recent_matches()
             self.assertEqual(mock_get.called, True)
 
     def test_get_epl_team_matches(self):
         with patch('api_caller.requests.get') as mock_get:
-            get_epl_team_matches("test")
+            api_caller.get_epl_team_matches("test")
             self.assertEqual(mock_get.called, True)
 
     def test_get_epl_team_matches_for_a_team(self):
@@ -31,7 +25,7 @@ class Tests(unittest.TestCase):
                 "matchday": "14",
                 "opposition": "Manchester City",
             }
-            response = get_epl_team_matches("Arsenal")
+            response = api_caller.get_epl_team_matches("Arsenal")
             self.assertEqual(response.text["id"], "57")
             self.assertEqual(response.text["team"], "Arsenal")
             self.assertEqual(response.text["matchday"], "14")
@@ -39,15 +33,15 @@ class Tests(unittest.TestCase):
 
     def test_get_epl_team_standings(self):
         with patch('api_caller.requests.get') as mock_get:
-            get_epl_team_standings()
+            api_caller.get_epl_team_standings()
             self.assertEqual(mock_get.called, True)
 
     def test_get_epl_top_scorers(self):
         with patch('api_caller.requests.get') as mock_get:
-            get_epl_top_scorers()
+            api_caller.get_epl_top_scorers()
             self.assertEqual(mock_get.called, True)
 
     def test_get_epl_matchday(self):
         with patch('api_caller.requests.get') as mock_get:
-            get_epl_matchday(23)
+            api_caller.get_epl_matchday(23)
             self.assertEqual(mock_get.called, True)
