0b196d9c167fa0087e22ec168dfc17bdf0e1e377
Debi Ejeta <86627766+Debi-Ejeta@users.noreply.github.com>
2023-03-24 10:17:18 -0400

    added more functions (#14)

--- soccer_api_wrapper/api_caller.py
+++ soccer_api_wrapper/api_caller.py
@@ -1,6 +1,5 @@
 import requests
 
-api_token = 'XXXXXXXXXXXXXXXXXXX'
 team_id_mapping = {
     57: 'Arsenal',
     65: 'Manchester City',
@@ -25,14 +24,14 @@ team_id_mapping = {
 }
 
 
-def get_recent_matches():
+def get_recent_matches(api_token):
     uri = 'https://api.football-data.org/v4/matches'
     headers = {'X-Auth-Token': api_token}
     response = requests.get(uri, headers=headers)
     return response.json()
 
 
-def get_epl_team_matches(team):
+def get_epl_team_matches(api_token, team):
     id = get_key(team, team_id_mapping)
     uri = f'https://api.football-data.org/v4/teams/{id}/ \
         matches?status=SCHEDULED'
@@ -41,23 +40,54 @@ def get_epl_team_matches(team):
     return response.json()
 
 
-def get_epl_team_standings():
+def get_epl_team_standings(api_token):
     uri = 'https://api.football-data.org/v4/competitions/PL/standings'
     headers = {'X-Auth-Token': api_token}
     response = requests.get(uri, headers=headers)
-    return response.json()["standings"][0]["table"]
+    try:
+        team_standings = response.json()["standings"][0]["table"]
+        return team_standings
+    except KeyError:
+        print("Maximum threshold reached")
+        return response
 
 
-def get_epl_top_scorers():
+def get_epl_top_scorers(api_token):
     uri = 'https://api.football-data.org/v4/competitions/PL/scorers'
     headers = {'X-Auth-Token': api_token}
     response = requests.get(uri, headers=headers)
-    return response.json()["scorers"]
+    try:
+        scorers = response.json()["scorers"]
+        return scorers
+    except KeyError:
+        print("Maximum threshold reached")
+        return response
 
 
-def get_epl_matchday(matchday):
+def get_epl_matchday(api_token, matchday):
     uri = f'https://api.football-data.org/v4/competitions/PL/matches? \
         matchday={matchday}'
+    headers = {'X-Auth-Token': api_token}
+    response = requests.get(uri, headers=headers)
+    return response.json()
+
+
+def get_team_info(api_token, team_id):
+    uri = f'http://api.football-data.org/v4/teams/{team_id}'
+    headers = {'X-Auth-Token': api_token}
+    response = requests.get(uri, headers=headers)
+    return response.json()
+
+
+def get_epl_teams(api_token):
+    uri = 'http://api.football-data.org/v4/competitions/PL/teams'
+    headers = {'X-Auth-Token': api_token}
+    response = requests.get(uri, headers=headers)
+    return response.json()
+
+
+def get_player_info(api_token, player_id):
+    uri = f'http://api.football-data.org/v4/persons/{player_id}'
     headers = {'X-Auth-Token': api_token}
     response = requests.get(uri, headers=headers)
     return response.json()
