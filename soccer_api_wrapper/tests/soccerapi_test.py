d4cdc83a62bbf2a05962f91e6c6a8710024fbfd7
Debi Ejeta <86627766+Debi-Ejeta@users.noreply.github.com>
2023-05-06 16:26:52 -0400

    Revert "added new features with tests (#26)"
    This reverts commit 98f7669ff271b54b14f57ecdededbfdb820c906d.

--- soccer_api_wrapper/tests/soccerapi_test.py
+++ soccer_api_wrapper/tests/soccerapi_test.py
@@ -9,7 +9,7 @@ class Tests(unittest.TestCase):
             soccerapi.get_recent_matches("test_token")
             self.assertEqual(mock_get.called, True)
 
-    def test_get_team_matches(self):
+    def test_get_epl_team_matches(self):
         with patch('requests.get') as mock_get:
             soccerapi.get_epl_team_matches("test_token", "test")
             self.assertEqual(mock_get.called, True)
@@ -46,49 +46,4 @@ class Tests(unittest.TestCase):
     def test_get_team_info(self):
         with patch('requests.get') as mock_get:
             soccerapi.get_team_info("test_token", 23)
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_competitions_for_team(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_competitions_for_team("test_token", 41)
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_players_of_team(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_players_of_team("test_token", 41)
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_player_by_position(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_player_by_position("test_token", 57, "Defender")
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_ucl_matches(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_ucl_matches("test_token")
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_matches(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_matches("test_token", "DED")
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_head_to_head_matches(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_head_to_head_matches("test_token", 43, 34)
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_head_to_head_stats(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_head_to_head_stats("test_token", 43, 34)
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_epl_teams(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_epl_teams("test_token")
-            self.assertEqual(mock_get.called, True)
-
-    def test_get_player_info(self):
-        with patch('requests.get') as mock_get:
-            soccerapi.get_player_info("test_token", 701)
             self.assertEqual(mock_get.called, True)
